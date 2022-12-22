2cba802f18b95e8fea1359ca27ae53c13242d9d6
Pete <p.eisenschmidt@gmail.com>
2022-12-22 15:13:26 +0100

    Second commit

--- thelab/models.py
+++ thelab/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200, blank=True)
+    initials = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.initials
